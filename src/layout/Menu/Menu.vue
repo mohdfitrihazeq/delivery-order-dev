0df6e94c5c9f53ab8e49a32e9293dcbe03eac19e
zylim726 <zylim@metrio.com.my>
2025-09-18 14:06:27 +0800

    update rename file

--- src/layout/Menu/Menu.vue
+++ src/layout/Menu/Menu.vue
@@ -1,20 +1,4 @@
-<script lang="ts">
-import MenuSeparator from '@/components/SeparatorLine.vue';
-import { defineComponent } from 'vue';
-import { useMenu } from './Menu';
-import MenuItem from './MenuItem.vue';
-
-export default defineComponent({
-    components: {
-        MenuItem,
-        MenuSeparator
-    },
-    setup() {
-        const { model } = useMenu();
-        return { model };
-    }
-});
-</script>
+<script lang="ts" src="./Menu.script.ts"></script>
 
 <template>
     <ul class="layout-menu">
@@ -24,5 +8,3 @@ export default defineComponent({
         </template>
     </ul>
 </template>
-
-<style scoped lang="scss"></style>
