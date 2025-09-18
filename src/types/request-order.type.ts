1cf2e6f0e4b017410a8f5c8ca45cbbd8fe4007ad
mohdfitrihazeq <phitriharxeq@gmail.com>
2025-09-18 17:36:52 +0800

    Adding new RO will pop up modal based on figma updates

--- src/types/request-order.type.ts
+++ src/types/request-order.type.ts
@@ -21,3 +21,28 @@ export interface BudgetOption {
     label: string;
     value: string;
 }
+
+export interface BudgetItem {
+    itemCode: string;
+    description: string;
+    location: string;
+    element: string;
+    itemType: string;
+    uom: string;
+    quantity: number;
+}
+
+export interface BudgetItem {
+    itemCode: string;
+    description: string;
+    location: string;
+    element: string;
+    itemType: string;
+    uom: string;
+    quantity: number;
+}
+
+export interface FilterOption {
+    label: string;
+    value: string;
+}
